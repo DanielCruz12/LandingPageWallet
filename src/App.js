0813418bd89075a024e44ac2db44b504605e8c73
daniel <danieldubonxd@gmail.com>
2022-06-19 16:38:41 -0600

    react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 //import logo from './logo.svg';
+import { Routes, Route, Link} from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Landing from "./pages/Landing";
 import assets from "./assets";
@@ -8,21 +9,23 @@ function App() {
   //const linkProject = "https://web.lappsnet.io/"
   return (
     <>
-      <LandingPage
+    <Routes>
+    <Route path="/" element={<LandingPage
         title="What is Lappsnet?"
         description=" It's an experimental smart contract network that can easily
               be used by lightning network users. You can purchase and redeem ESAT tokens from"  
       
         mockupImg={assets.first}
         banner="banner"
-      />
+      />}/>
 
-      <Landing title="How does the wallet work?"
+      <Route path="/landing" element={<Landing title="How does the wallet work?"
         description=" The wallet generates a key, which is encrypted using a security
               device, such as your screen lock. The encrypted key is stored in
               the browser."
         mockupImg={assets.first}
-        reverse/>
+        reverse/>}/>
+        </Routes>
     </>
   );
 }
